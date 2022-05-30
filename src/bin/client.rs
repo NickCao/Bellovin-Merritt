c8fc7cc4576c15ccdf64c4c6b90b739807be1b60
Nick Cao <nickcao@nichi.co>
2022-05-30 15:09:24 +0800

    client: simplify log messages

--- src/bin/client.rs
+++ src/bin/client.rs
@@ -7,43 +7,42 @@ use std::net::TcpStream;
 
 fn main() {
     SimpleLogger::new().init().unwrap();
+    log::info!("connecting to server");
     let mut stream = TcpStream::connect("127.0.0.1:34254").unwrap();
     log::info!("connected to server");
 
-    // step 1
     let mut rng = rand::thread_rng();
     let sk = RsaPrivateKey::new(&mut rng, 1024).unwrap();
     let pk = RsaPublicKey::from(&sk);
     log::info!("generated keypair");
 
-    let id = [0x42; 8]; // TODO: make id configurable
-    let pw = [0x42; 32]; // TODO; make pw configurable
-    let buffer = pk.to_public_key_der().unwrap().as_ref().to_owned();
+    let id: [u8; 8] = rand::random();
     stream.write_all(&id).unwrap();
-    chacha20_send(pw, &buffer, &mut stream).unwrap();
-    log::info!("sent identity and encrypted publickey to server");
+    log::info!("sent randomly generated identity to server");
 
-    // step 3
-    let ks_vec = chacha20_recv(pw, &mut stream).unwrap();
-    let ks_vec = sk
-        .decrypt(PaddingScheme::new_pkcs1v15_encrypt(), &ks_vec)
+    let pw = [0x42; 32];
+    chacha20_send(pw, pk.to_public_key_der().unwrap().as_ref(), &mut stream).unwrap();
+    log::info!("sent publickey to server");
+
+    let ks = chacha20_recv(pw, &mut stream).unwrap();
+    let ks = sk
+        .decrypt(PaddingScheme::new_pkcs1v15_encrypt(), &ks)
         .unwrap();
-    let mut ks = [0u8; 32];
-    ks.copy_from_slice(&ks_vec);
-    log::info!("decrypted session key");
+    let ks: [u8; 32] = ks.try_into().unwrap();
+    log::info!("got session key from server");
 
     let na: [u8; 8] = rand::random();
     chacha20_send(ks, &na, &mut stream).unwrap();
-    log::info!("sent encrypted na to server");
+    log::info!("sent na to server");
 
     let nanb = chacha20_recv(ks, &mut stream).unwrap();
     assert_eq!(nanb[..8], na);
-    log::info!("verified na from server");
+    log::info!("got na from server");
 
     chacha20_send(ks, &nanb[8..], &mut stream).unwrap();
-    log::info!("sent encrypted nb to server");
+    log::info!("sent nb to server");
 
     let message = "hello Bellovin-Merritt".as_bytes().to_vec();
     chacha20_send(ks, &message, &mut stream).unwrap();
-    log::info!("sent encrypted message to server");
+    log::info!("sent message to server");
 }
